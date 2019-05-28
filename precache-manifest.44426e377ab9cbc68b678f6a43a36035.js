6367babdc510ddef07f1fcfff33ac214a412a355
Marcel Bednarz <marcel.bednarz16@gmail.com>
2019-05-28 20:17:43 +0200

    Updates

---
+++ precache-manifest.44426e377ab9cbc68b678f6a43a36035.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9467298755cc22cd37fc26b2cb637b39",
+    "url": "/ComNet/index.html"
+  },
+  {
+    "revision": "cee9c63f67530ab8ddd5",
+    "url": "/ComNet/static/css/2.79692622.chunk.css"
+  },
+  {
+    "revision": "cee9c63f67530ab8ddd5",
+    "url": "/ComNet/static/js/2.3ac6e7ae.chunk.js"
+  },
+  {
+    "revision": "7b8f4a12060775101718",
+    "url": "/ComNet/static/js/main.457b3a65.chunk.js"
+  },
+  {
+    "revision": "7d47ddd1c1f5aaf2fa8e",
+    "url": "/ComNet/static/js/runtime~main.2682f8dd.js"
+  },
+  {
+    "revision": "aa5dec35477efb79def88a3858ca014a",
+    "url": "/ComNet/static/media/1.aa5dec35.png"
+  },
+  {
+    "revision": "7d9fb876376fdd5f80aa7f0358599ffb",
+    "url": "/ComNet/static/media/2.7d9fb876.png"
+  },
+  {
+    "revision": "3cd58738e05a1b055e99ae4870d6600c",
+    "url": "/ComNet/static/media/3.3cd58738.png"
+  },
+  {
+    "revision": "fc87bf76c13c688b2f3af70a8f95e3dd",
+    "url": "/ComNet/static/media/4.fc87bf76.png"
+  },
+  {
+    "revision": "ff8daf8ea207c038ca6f1eaaad38c442",
+    "url": "/ComNet/static/media/5.ff8daf8e.png"
+  },
+  {
+    "revision": "67037c6ee0334e970c87e4ee3d20a62a",
+    "url": "/ComNet/static/media/6.67037c6e.png"
+  },
+  {
+    "revision": "bb43723f1344312bd72b6d41c50e32c3",
+    "url": "/ComNet/static/media/7.bb43723f.png"
+  },
+  {
+    "revision": "629c2f79d860a0b6c6d5f0638742732e",
+    "url": "/ComNet/static/media/8.629c2f79.png"
+  },
+  {
+    "revision": "4227516c4ecade7150fa83fb55c8ef9d",
+    "url": "/ComNet/static/media/Fala2.4227516c.png"
+  },
+  {
+    "revision": "fe931bc71fdd3a1b96b801c1084264a2",
+    "url": "/ComNet/static/media/computer.fe931bc7.png"
+  }
+]);
